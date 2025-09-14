41b658feb34db7b76d64716502d0266cbb23a8e8
M. fajal <fajalmoaj@gmail.com>
2025-09-14 13:23:50 +0530

    exception

---
+++ src/exception.py
@@ -0,0 +1,16 @@
+import sys
+
+def error_message_details(error, error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = "Error occured in python script name [{0}], line no [{1}], error messege[{2}]".format(
+        file_name, exc_tb.tb_lineno,str(error)
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        
