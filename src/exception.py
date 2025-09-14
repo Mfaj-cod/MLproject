c07562f7a346a7adf73fc784d8f219280cea0d4b
M. fajal <fajalmoaj@gmail.com>
2025-09-14 14:09:09 +0530

    loggeing and exception

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 def error_message_details(error, error_detail:sys):
@@ -13,4 +14,8 @@ def error_message_details(error, error_detail:sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        
+        self.error_message=error_message_details(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
