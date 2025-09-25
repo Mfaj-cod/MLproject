75f124337457caf9570bf26541652683ae4a2dcc
M. fajal <fajalmoaj@gmail.com>
2025-09-26 03:13:46 +0530

    UI

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,13 +48,80 @@ class ModelTrainer:
                 "CatBoost": CatBoostRegressor(),
                 "AdaBoost": AdaBoostRegressor()
             }
+
+
+            params = {
+                "Random Forest": {
+                    'n_estimators': [100, 200, 300],
+                    'max_depth': [None, 5, 10, 20],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4],
+                    'max_features': ['sqrt', 'log2']
+                },
+
+                "Decision tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_depth': [None, 5, 10, 20],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4]
+                },
+
+                "Gradient boosting": {
+                    'n_estimators': [100, 200, 300],
+                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                    'max_depth': [3, 5, 7, 10],
+                    'subsample': [0.8, 1.0],
+                    'min_samples_split': [2, 5, 10]
+                },
+
+                "Linear regression": {
+                    # Mostly parameter-free, but these are optional
+                    'fit_intercept': [True, False],
+                    'positive': [True, False]
+                },
+
+                "K-nearest neighbours": {
+                    'n_neighbors': [3, 5, 7, 9, 11],
+                    'weights': ['uniform', 'distance'],
+                    'metric': ['euclidean', 'manhattan', 'minkowski']
+                },
+
+                "XGBoost": {
+                    'n_estimators': [100, 200, 300],
+                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                    'max_depth': [3, 5, 7, 10],
+                    'subsample': [0.8, 1.0],
+                    'colsample_bytree': [0.8, 1.0],
+                    'reg_alpha': [0, 0.01, 0.1],
+                    'reg_lambda': [1, 1.5, 2.0]
+                },
+
+                "CatBoost": {
+                    'iterations': [200, 500],
+                    'depth': [4, 6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'l2_leaf_reg': [1, 3, 5, 7, 9]
+                    # Note: CatBoost handles categorical automatically
+                },
+
+                "AdaBoost": {
+                    'n_estimators': [50, 100, 200],
+                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
+                    'loss': ['linear', 'square', 'exponential']
+                }
+            }
+
+
+
+
             # calling the evaluate function from utils.py
             model_report:dict=evaluate_model(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params=params
                 )
             
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import dill
 
 # functiong for saving any object to the specific path
@@ -21,11 +22,17 @@ def save_object(file_path, obj):
     
 
 # fucntion for evaluating the model
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param_grid=param, cv=5)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
