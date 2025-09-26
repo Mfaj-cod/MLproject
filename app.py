98833118996aef7bd7378335d20f23789ef87a56
M. fajal <fajalmoaj@gmail.com>
2025-09-26 15:31:57 +0530

    take input and predict

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 app = Flask(__name__)
 
@@ -15,5 +16,24 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        data=CustomData
-    
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation'),
+            reading_score=request.form.get('reading_score'),
+            writing_score=request.form.get('writing_Score')
+        )
+
+        preds_df=data.get_data_as_dataframe()
+        print(preds_df)
+
+        predict_pipeline=PredictPipeline()
+        results=predict_pipeline.predict(preds_df)
+
+        return render_template('home.html', results=results[0])
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=True)
